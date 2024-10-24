27201a13558651c279b74bbb5b5490711819f3e2
xxlindatrinh <145966260+xxlindatrinh@users.noreply.github.com>
2024-10-24 14:02:52 +0200

    Update Forside.jsx

--- src/Forside.jsx
+++ src/Forside.jsx
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
-<html lang="da">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link 
-        href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200..900&display=swap" rel="stylesheet">
-    <meta name="description" content="Portfolio for multimediedesigner [Dit Navn]. Se mine projekter og tjenester indenfor webdesign, UX/UI, grafisk design og multimedieløsninger.">
-    <meta name="keywords" content="multimediedesigner, webdesign, UX, UI, grafisk design, webudvikling, designløsninger">
-    <meta name="author" content="[Dit Navn]">
-    <meta property="og:title" content="[Dit Navn] | Multimediedesigner Portfolio">
-    <meta property="og:description" content="Portfolio for multimediedesigner [Dit Navn]. Se mine projekter og tjenester indenfor webdesign, UX/UI, grafisk design og multimedieløsninger.">
-    <meta property="og:image" content="[URL til billede]">
-    <meta property="og:url" content="[URL til din portfolio]">
-    <meta property="og:type" content="website">
-    <title></title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+function Onboarding () {
 
 
-</body>
+}
 
-<footer class="footer">
+function Forside() {
+    return (
+        <>
+        <Onboarding />
+        </>
+    );
+}
 
-</footer>
-</html>
+
+export default Forside;
