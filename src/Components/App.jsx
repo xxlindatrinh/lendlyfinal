18b74683d2b7e0baf15a2444b570b2b4ad664ccf
vohejl <148538744+vohejl@users.noreply.github.com>
2024-10-28 16:56:33 +0100

    update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,40 +1,45 @@
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import '../css/App.css';
-import Forside from './Forside';
-import Kategori from './Kategori';
-import Upload from './Upload';
-import Profil from './Profil';
-import OpretProfil from './OpretProfil';
-import BottomNav from './BottomNav';
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import "../css/App.css";
+import Forside from "./Forside";
+import Kategori from "./Kategori";
+import Upload from "./Upload";
+import Profil from "./Profil";
+import OpretProfil from "./OpretProfil";
+import BottomNav from "./BottomNav";
+import Opretprofil from "./Opretprofil/Opretprofil";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Main />
-        </BrowserRouter>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+
+      <Opretprofil />
+    </>
+  );
 }
 
 function Main() {
-    const location = useLocation(); // Get the current location
+  const location = useLocation(); // Get the current location
 
-    // Specify the routes where BottomNav should NOT appear
-    const noBottomNavRoutes = ['/OpretProfil']; // BottomNav will not appear on this route
+  // Specify the routes where BottomNav should NOT appear
+  const noBottomNavRoutes = ["/OpretProfil"]; // BottomNav will not appear on this route
 
-    const showBottomNav = !noBottomNavRoutes.includes(location.pathname);
+  const showBottomNav = !noBottomNavRoutes.includes(location.pathname);
 
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Forside />} />
-                <Route path='/Kategori' element={<Kategori />} />
-                <Route path='/Upload' element={<Upload />} />
-                <Route path='/Profil' element={<Profil />} />
-                <Route path='/OpretProfil' element={<OpretProfil />} />
-            </Routes>
-            {showBottomNav && <BottomNav />} {/* Conditionally render BottomNav */}
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Forside />} />
+        <Route path="/Kategori" element={<Kategori />} />
+        <Route path="/Upload" element={<Upload />} />
+        <Route path="/Profil" element={<Profil />} />
+        <Route path="/OpretProfil" element={<OpretProfil />} />
+      </Routes>
+      {showBottomNav && <BottomNav />} {/* Conditionally render BottomNav */}
+    </>
+  );
 }
 
 export default App;
