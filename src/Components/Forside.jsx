63fc9b7a0245a6482151e0205f13d1efabf2ff52
xxlindatrinh <145966260+xxlindatrinh@users.noreply.github.com>
2024-10-29 21:55:37 +0100

    forside kodning

--- src/Components/Forside.jsx
+++ src/Components/Forside.jsx
@@ -3,11 +3,27 @@ import "../css/Forside.css";
 function Forside() {
   return (
     <>
-    <div className='forside-kategori-scroll'>
-      <h1>
-        Horisontal scroll
-      </h1>
-    </div>
+      <div className="forside-container">
+        <h1>I nærheden af dig</h1>
+        <div className="grid-container">
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Bog</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Stige</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Plæneklipper</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Boremaskine</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
