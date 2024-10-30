687b2a1a3148ad75a16ccf72d464367689ea8096
xxlindatrinh <145966260+xxlindatrinh@users.noreply.github.com>
2024-10-30 09:10:32 +0100

    forside kodning

--- src/Components/Forside.jsx
+++ src/Components/Forside.jsx
@@ -3,6 +3,44 @@ import "../css/Forside.css";
 function Forside() {
   return (
     <>
+
+<div>
+            <h1>Horizontal Scroll Categories</h1>
+            <div>
+                <a href="#">
+                    <span>
+                        <span>Hus og have</span>
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                        <span>Værktøj</span>
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                        <span>Elektronik</span>
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                        <span>Tøj</span>
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                        <span>Hobby</span>
+                    </span>
+                </a>
+                <a href="#">
+                    <span>
+                        <span>Diverse</span>
+                    </span>
+                </a>
+
+            </div>
+        </div>
+
       <div className="forside-container">
         <h1>I nærheden af dig</h1>
         <div className="grid-container">
@@ -21,6 +59,22 @@ function Forside() {
           <div className="grid-item">
             <div className="image-placeholder"></div>
             <p>Boremaskine</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Badminton ketcher</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Strikkepinde sæt</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>Sækkevogn</p>
+          </div>
+          <div className="grid-item">
+            <div className="image-placeholder"></div>
+            <p>4 stole</p>
           </div>
         </div>
       </div>
