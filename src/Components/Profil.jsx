fe7d9a7cef76b8397f247c8ca68a9b78660a1274
vohejl <148538744+vohejl@users.noreply.github.com>
2024-11-04 12:54:39 +0100

    update profil

--- src/Components/Profil.jsx
+++ src/Components/Profil.jsx
@@ -1,59 +1,57 @@
-import '../css/Profil.css';
-import { Link, useNavigate } from 'react-router-dom';
-
-
+import "../css/Profil.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function Profil() {
-    const navigate = useNavigate();
-    return (
-        <>
-        <div className="profil-container">
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="profil-container">
         <header className="profil-header">
-        <div id='back-button'>
-                <button className="back-button" onClick={() => navigate(-1)}>&lt; Tilbage</button>
-                </div>
-                <div>
-                <h1>Profil</h1>
-                </div>
-                <div>
-                </div>
-            </header>
-            
-            <Link to="/Omprofil" className="profile-info">
-            <div className="profile-picture">
-                <img src="ikoner/profil.svg" alt="Profile" />
-            </div>
-            <div className="profile-name">
-                <p>Mit navn</p>
-                <span>Vis min profil</span>
-            </div>
-            </Link>
+          <div id="back-button">
+            <button className="back-button" onClick={() => navigate(-1)}>
+              &lt; Tilbage
+            </button>
+          </div>
+          <div>
+            <h1>Profil</h1>
+          </div>
+          <div></div>
+        </header>
 
-            <div className="profile-options">
-                <button className="option-button">Din guide til Lendly</button>
-                <Link to="/Opslag" className="option-button">
-                    Mine opslag
-                </Link>
+        <Link to="/Omprofil" className="profile-info">
+          <div className="profile-picture">
+            <img src="img/profilbillede.svg" alt="Profile" />
+          </div>
+          <div className="profile-name">
+            <p>Mit navn</p>
+            <span>Vis min profil</span>
+          </div>
+        </Link>
 
-                <button className="option-button">Mine lejeaftaler</button>
-                <button className="option-button">Mine udlejninger</button>
-                <button className="option-button">Brugertilpasning</button>
-                <button className="option-button">Mine grupper</button>
-            </div>
+        <div className="profile-options">
+          <button className="option-button">Din guide til Lendly</button>
+          <Link to="/Opslag" className="option-button">
+            Mine opslag
+          </Link>
 
-            <div className="profile-extras">
-                <button className="option-button">Inviter venner</button>
-                <button className="option-button">Hjælpecenter</button>
-                <button className="option-button">Om Lendly</button>
-            </div>
-
-            <div className="profile-holiday">
-                <button className="option-button">Ferie­tilstand</button>
-            </div>
+          <button className="option-button">Mine lejeaftaler</button>
+          <button className="option-button">Mine udlejninger</button>
+          <button className="option-button">Brugertilpasning</button>
+          <button className="option-button">Mine grupper</button>
         </div>
-        </>
-    );
+
+        <div className="profile-extras">
+          <button className="option-button">Inviter venner</button>
+          <button className="option-button">Hjælpecenter</button>
+          <button className="option-button">Om Lendly</button>
+        </div>
+
+        <div className="profile-holiday">
+          <button className="option-button">Ferie­tilstand</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Profil;
-
