61f0f94cdaef5db14fdc48ec699e60faa403447c
xxlindatrinh <145966260+xxlindatrinh@users.noreply.github.com>
2024-11-04 11:50:53 +0100

    favoritter siden

--- src/Components/Favoritter.jsx
+++ src/Components/Favoritter.jsx
@@ -3,7 +3,7 @@ import '../css/Favoritter.css';
 function Favoritter() {
     return (
         <div className="favoritter-container">
-            <h1>I nærheden af dig</h1>
+            <h1>Mine favoritter</h1>
             <div className="grid-container">
                 <div className="grid-item">
                     <img src="img/sækkevogn.webp" alt="Sækkevogn" className="image-placeholder" />
@@ -11,24 +11,9 @@ function Favoritter() {
                     <p>30 kr</p>
                 </div>
                 <div className="grid-item">
-                    <img src="/img/badminton.jpg" alt="Stige" className="image-placeholder" />
-                    <p>Stige</p>
-                    <p>20 kr</p>
-                </div>
-                <div className="grid-item">
-                    <img src="img/strikkepinde.webp" alt="Strikkepinde sæt" className="image-placeholder" />
-                    <p>Strikkepinde sæt</p>
-                    <p>40 kr</p>
-                </div>
-                <div className="grid-item">
-                    <img src="img/trillebør.webp" alt="Trillebør" className="image-placeholder" />
-                    <p>Trillebør</p>
-                    <p>30 kr</p>
-                </div>
-                <div className="grid-item">
-                    <img src="img/sav.jpg" alt="Geringsav" className="image-placeholder" />
-                    <p>Geringsav</p>
-                    <p>20 kr</p>
+                <img src="img/soundboks.webp" alt="Soundboks" className="image-placeholder" />
+                <p>Soundboks</p>
+                <p>300 kr</p>
                 </div>
                 <div className="grid-item">
                     <div className="image-container">
@@ -39,19 +24,9 @@ function Favoritter() {
                     <p>50 kr</p>
                 </div>
                 <div className="grid-item">
-                    <img src="img/elektrisk-høvl.webp" alt="Elektrisk høvl" className="image-placeholder" />
-                    <p>Elektrisk høvl</p>
-                    <p>50 kr</p>
-                </div>
-                <div className="grid-item">
-                    <img src="img/stole.jpg" alt="4 stole" className="image-placeholder" />
+                    <img src="img/stole.webp" alt="4 stole" className="image-placeholder" />
                     <p>4 stole</p>
                     <p>30 kr</p>
-                </div>
-                <div className="grid-item">
-                    <img src="img/strikkepinde.webp" alt="Strikkepinde sæt" className="image-placeholder" />
-                    <p>Strikkepinde sæt</p>
-                    <p>40 kr</p>
                 </div>
             </div>
         </div>
--- src/Components/Opslag.jsx
+++ src/Components/Opslag.jsx
@@ -6,6 +6,4 @@ function Opslag() {
         </>
     );
 }
-
-
 export default Opslag;
